d63846da57fc31b98b0830498d1f6b5db5ebc4f2
TaehyeonPark <84885011+TaehyeonPark@users.noreply.github.com>
2023-07-26 14:50:57 +0900

    Update phpldapadmininstaller.sh

--- phpldapadmininstaller.sh
+++ phpldapadmininstaller.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-apt install -y apache2 php7.2 php7.2-cgi libapache2-mod-php7.2 php7.2-mbstring php7.2-common php7.2-xml php-pear php7.2-ldap
-a2enconf php7.2-cgi
+VERSION=7.2
+
+apt install -y apache2 php$VERSION php$VERSION-cgi libapache2-mod-php$VERSION php$VERSION-mbstring php$VERSION-common php$VERSION-xml php-pear php$VERSION-ldap
+a2enconf php$VERSION-cgi
 
 git clone https://github.com/breisig/phpLDAPadmin.git /var/www/html/phpldapadmin
 cp /var/www/html/phpldapadmin/config/config.php.example /var/www/html/phpldapadmin/config/config.php
